50cdec5e7a7fdedfcf245d439bee0ff0c9f09bbc
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-03-13 11:43:21 +0100

    Polygon

---
+++ hello_template/src/Contract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+contract Contract {
+  function Hello() public pure returns (string memory) { 
+      return "Hello From Polygon"; 
+  }
+}
+// 0xbafd08ecc871c6963449273e9201275421837c57912e567b873a4a037e59fb59 private key
+// Address 0xbEeF08F756f0f930318c67fb9A0CBCfCC0A33C79
+// Deployed to 0x407Fae1ad99A6cCCe9EBcb6517bf371dfCa9c4f9
+// Harsh 0x5ee645a03e29c3181a97b9f99c060f4de3b4642ac4ba8aada3301c0269651409
